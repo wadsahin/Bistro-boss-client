d6916b913047ef4fdd1be3bb6b15eacff8c07bac
wadsahin <wadsahinalom@gmail.com>
2025-01-02 09:58:02 +0600

    Created a custom hook named useMenu for loading all menu items & show popular menu in home page

--- src/Components/Home/HomeMenuSec.jsx
+++ src/Components/Home/HomeMenuSec.jsx
@@ -3,21 +3,18 @@ import img1 from "../../assets/menu/salad-bg.jpg"
 import img2 from "../../assets/menu/soup-bg.jpg"
 import img3 from "../../assets/menu/pizza-bg.jpg"
 import img4 from "../../assets/menu/dessert-bg.jpeg"
+import useMenu from "../../Hooks/useMenu";
 
 const HomeMenuSec = () => {
+  const [menu] = useMenu();
+  const popular = menu.filter(item => item.category === "popular");
+  console.log(popular);
   return (
     <div className="mb-10">
-      <div className="md:flex gap-10 px-5">
-        <MenuItemSlat img={img1} title="Yummy Salads" price="11.4" />
-        <MenuItemSlat img={img2} title="Crazy Hot Soup" price="14.6" />
-      </div>
-      <div className="md:flex gap-10 px-5">
-        <MenuItemSlat img={img3} title="Crezzy Pizza" price="11.4" />
-        <MenuItemSlat img={img4} title="Lovely Dessert" price="14.6" />
-      </div>
-      <div className="md:flex gap-10 px-5">
-        <MenuItemSlat img={img2} title="Yummy Soup" price="11.4" />
-        <MenuItemSlat img={img1} title="Authentic Salad" price="14.6" />
+      <div className="grid gap-5 md:grid-cols-2">
+        {
+          popular.map(item => <MenuItemSlat key={item._id} item={item}  />)
+        }
       </div>
     </div>
   );
--- src/Components/common/MenuItemSlat.jsx
+++ src/Components/common/MenuItemSlat.jsx
@@ -1,11 +1,12 @@
 
-const MenuItemSlat = ({img, title, price}) => {
+const MenuItemSlat = ({item}) => {
+  const {name, image, price, recipe } = item;
   return (
-    <div className="flex gap-3 w-1/2 my-5 ">
-      <img src={img} className="w-28" style={{borderRadius: "0 200px 200px 200px" }} alt="" />
+    <div className="flex gap-3 my-5 ">
+      <img src={image} className="w-28" style={{borderRadius: "0 200px 200px 200px" }} alt="" />
       <div>
-        <h3 className="uppercase font-medium mb-1">{title} ------------------</h3>
-        <p className="text-gray-500">Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce</p>
+        <h3 className="uppercase font-medium mb-1">{name} ------------------</h3>
+        <p className="text-gray-500">{recipe}</p>
       </div>
       <span className="text-orange-600 font-medium">${price}</span>
     </div>
